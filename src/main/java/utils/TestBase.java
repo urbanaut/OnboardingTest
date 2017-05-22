11f4ab9fce29ef5d1c1930fbf75d7154e6159c7b
urbanaut <bwitt4@gmail.com>
2017-05-22 11:52:07 -0600

    Added jenkins chromedriver path to TestBase class, fix

--- src/main/java/utils/TestBase.java
+++ src/main/java/utils/TestBase.java
@@ -26,7 +26,7 @@ public class TestBase {
     protected static WebDriverWait wait;
     protected static JavascriptExecutor jsEx;
     protected static Actions action;
-    public static String driverPathJenkins = "$workspace/src/main/resources/drivers/chromedriver.exe";
+    public static String driverPathJenkins = "/src/main/resources/drivers/chromedriver.exe";
     public static String driverPathDev = "src\\main\\resources\\drivers\\chromedriver.exe";
     public static String startingUrlProd = "http://onboarding.stgconsulting.com/new-user#/";
     public static String startingUrlDev = "http://10.117.3.200:8111";
